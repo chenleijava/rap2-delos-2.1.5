48241d980a18fb95db9d896197b5b0571bf414d7
石头哥哥 <chenleijava@gmail.com>
2019-09-10 21:05:25 +0800

    password

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -29,7 +29,6 @@ let config: IConfigOptions =  {
     redis: {
         host: process.env.REDIS_URL || 'localhost',
         port: (process.env.REDIS_PORT && parseInt(process.env.REDIS_PORT)) || 6379,
-        auth_pass: process.env.REDIS_PASSWORD||'HorO^($(9496sCopE',
     },
     mail: {
       host: 'smtp-mail.outlook.com',
